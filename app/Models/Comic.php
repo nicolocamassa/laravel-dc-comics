47e68b1311615cef2f846e3a76f883e2f124a55e
nicolocamassa <camassanicolo03@gmail.com>
2024-02-16 14:26:27 +0100

    [ADD] file setup complete

---
+++ app/Models/Comic.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\ComicController as ComicController;
+
+class Comic extends Model
+{
+    use HasFactory;
+}
